0914cdd87d6a460d8d2680f57fb2352de638621e
Alishba-shahzad <alishbah722@gmail.com>
2025-12-13 09:59:24 +0000

    datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,17 @@
+// let age = 22
+// let address = "123 main road"
+// console.log(typeof age);
+// consolelog(typeof address);
+
+let age = "22"
+console.log(typeof age)
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1 => true , 0 => false
+// "" => false , "abc" => true
