f5df18b78cecff5931485f3679f1314c94a754a5
Alishba-shahzad <alishbah722@gmail.com>
2025-12-14 17:11:28 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,28 @@
+const arr = [1,2,3,4,5]
+// console.log(arr)
+// console.log(arr.length)
+
+
+arr.push(6,7)
+
+console.log(arr)
+
+// arr.unshift(8)
+// arr.shift()
+
+console.log(arr.includes(8))
+console.log(arr.indexOf(3))
+
+// splice & slice
+
+console.log("A", arr)
+
+const newArr = arr.slice(1,3)
+console.log(newArr)
+console.log("B", newArr)
+// console.log(newArr)
+
+const newArr2 = arr.splice(1,3)
+console.log("C", newArr)
+console.log(newArr2)
+
